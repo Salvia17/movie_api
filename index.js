97176777dbd611713e45b520fb33cf3dd118c782
Salvia17 <salvija13@gmail.com>
2021-01-08 12:17:14 +0000

    Update index.js
    Error-handling middleware function, and movie list

--- index.js
+++ index.js
@@ -7,12 +7,60 @@ app.use(morgan('common'));
 
 app.use(express.static('public'));
 
+let topTenMovies = [
+  {
+    title: 'The Godfather',
+    cast: 'Marlon Brando, Al Pacino, James Caan, Robert Duvall, Diane Keaton, Talia Shire'
+  },
+  {
+    title: 'The Wizard of Oz',
+    cast: 'Judy Garland, Frank Morgan, Ray Bolger, Bert Lahr, Jack Haley'
+  },
+  {
+    title: 'Citizen Kane',
+    cast: 'Orson Welles, Joseph Cotten, Dorothy Comingore, Agnes Moorehead, Ruth Warrick'
+  },
+  {
+    title: 'The Shawshank Redemption',
+    cast: 'Tim Robbins, Morgan Freeman'
+  },
+  {
+    title: 'Pulp Fiction',
+    cast: 'John Travolta, Uma Thurman, Samuel L. Jackson, Bruce Willis, Ving Rhames'
+  },
+  {
+    title: 'Casablanca',
+    cast: 'Humphrey Bogart, Ingrid Bergman'
+  },
+  {
+    title: 'The Godfather: Part II',
+    cast: 'Al Pacino, Robert De Niro, Robert Duvall, Diane Keaton'
+  },
+  {
+    title: 'E.T. The Extra-Terrestrial',
+    cast: 'Henry Thomas, Drew Barrymore, Dee Wallace'
+  },
+  {
+    title: '2001: A Space Odyssey',
+    cast: 'Keir Dullea, Gary Lockwood, William Sylvester'
+  },
+  {
+    title: "Schindler's List",
+    cast: 'Liam Neeson, Ralph Fiennes, Ben Kingsley'
+  },
+];
+
 app.get('/movies', (_req, res) => {
   res.json(topTenMovies);
 });
 
 app.get('/', (_req, res) => {
   res.send('Welcome to myFlix!');
+});
+
+app.use((err, _req, res, _next) => {
+  console.error(err.stack);
+  res.status(500).send('Uh Oh! Something went wrong!');
 });
 
 app.listen(8080, () =>
