59d96fddd84b3dabc9c9c79abca4cc7a48070765
Salvia17 <>
2021-01-08 11:08:29 +0000

    Creting get routes

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.get('/movies', (req, res) => {
+  res.json(topTenMovies);
+});
+
+app.get('/', (req, res) =>{
+  res.send('Welcome to myFlix!');
+});
+
 app.listen(8080, () =>
   console.log('Your app is listening on port 8080.'));
