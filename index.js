5c70c4a8651fdef602ac706dd4d7e1550030c506
Salvia17 <salvija13@gmail.com>
2021-01-09 10:37:14 +0000

    Routing all end points
    Handler methods haven't been implemented so far.

--- index.js
+++ index.js
@@ -50,12 +50,55 @@ let topTenMovies = [
   },
 ];
 
-app.get('/movies', (_req, res) => {
-  res.json(topTenMovies);
-});
-
 app.get('/', (_req, res) => {
   res.send('Welcome to myFlix!');
+});
+
+//Return a list of ALL movies
+app.get('/movies', (_req, res) => {
+  res.send('Successful GET request returning data on all the movies');
+});
+
+//Return data about a single movie by title
+app.get('/movies/:title', (_req, res) => {
+  res.send('Successful GET request returning data on single movie' + req.params.title);
+});
+
+//Return data about a genre (description)
+app.get('/movies/genres/:genre', (_req, res) => {
+  res.send('Successful GET request returning data on movie genre' + req.params.genre);
+});
+
+//Return data about a director
+app.get('/movies/directors/:name', (_req, res) => {
+  res.send('Successful GET request returning data on single director' + req.params.name);
+});
+
+//POST request to allow new users to register
+app.post('/users', (_req, res) => {
+  res.send('Successful POST request registering new user');
+});
+
+//Allow users to update their user info (username)
+app.put('/users/:username', (_req, res) => {
+  res.send('Successful PUT request updating username' + req.params.username);
+});
+
+//Allow users to add a movie to their list of favorites
+app.post('/users/:username/favourites/:title', (_req, res) => {
+  res.send('Successful POST request adding movie by title'
+  + req.params.title + 'to list of favourites' + req.params.username);
+});
+
+//Allow users to remove a movie from their list of favorites
+app.delete('/users/:username/favourites/:title', (_req, res) => {
+  res.send('Successful DELETE request removing movie by title'
+  + req.params.title + 'from list of favourites' + req.params.username);
+});
+
+//Allow existing users to deregister 
+app.delete('/users/:username', (_req, res) => {
+  res.send('Successful DELETE request removing user: ' + req.params.username + ' from database');
 });
 
 app.use((err, _req, res, _next) => {
