df2b0e316ba296420b3b6d2566f9285dbba76228
Salvija Dargyte <salvija13@gmail.com>
2021-05-18 19:05:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,12 @@ app.use(
   })
 );
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Welcome to myFlix!');
 });
