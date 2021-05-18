142831c7f6b2fbd53522fda59c61a818e74d4680
Salvija Dargyte <salvija13@gmail.com>
2021-05-18 20:55:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", '*',
-    "http://localhost:4200/");
+    "http://localhost:4200");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
