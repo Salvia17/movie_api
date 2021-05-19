c2a80ab9a6c155857b8d2313177d63b2f2204e10
Salvija Dargyte <salvija13@gmail.com>
2021-05-19 06:34:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,18 +26,18 @@ let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 
-const cors = require('cors');
+/*const cors = require('cors');
 /*app.use(
   cors({
     allowedHeaders: '*',
   })
-);*/
+);
 
 app.all('/', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next()
-});
+});*/
 
 
 app.use((err, req, res, next) => {
